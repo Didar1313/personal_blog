769823fe63feb69cc7bfc3dce93928f7e51e7f98
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-07-10 18:46:27 +0600

    feat: add method for getting each post by their id

--- lib/service.dart
+++ lib/service.dart
@@ -1,18 +1,33 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:personal_blog/post.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.0.198:8080';
+  static const String baseUrl = 'http://192.168.0.198:8080/api';
 
   static Future<List<Post>> fetchPosts() async {
-    final response = await http.get(Uri.parse('$baseUrl/api/posts'));
-
+    final response = await http.get(Uri.parse('$baseUrl/posts'));
     if (response.statusCode == 200) {
-      List jsonData = json.decode(response.body);
-      return jsonData.map((e) => Post.fromJson(e)).toList();
+      final List data = json.decode(response.body);
+      return data.map((e) => Post.fromJson(e)).toList();
     } else {
       throw Exception('Failed to load posts');
     }
   }
+
+  static Future<Post> fetchPostById(int id) async {
+    final url = '$baseUrl/posts/$id';
+    print("Requesting: $url");
+
+    final response = await http.get(Uri.parse(url));
+    print("Status Code: ${response.statusCode}");
+    print("Body: ${response.body}");
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
 }
